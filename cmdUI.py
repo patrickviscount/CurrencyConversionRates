90630a0886faa61ba2ffea82b0e879b87d9f8369
Patrick Viscount <71523753+patrickviscount@users.noreply.github.com>
2024-01-31 10:41:09 -0400

    Create cmdUI.py

---
+++ cmdUI.py
@@ -0,0 +1,11 @@
+def main():
+    options = ["American Dollar", "Mexican Peso", "British Pound"]
+    print(f"Welcome, what currency do you want to select: {options}!")
+    #command line Ui menu here, 1. American dollar, 2. peso, etc
+    print(f"Would you like to convert into Canadian dollars or not?")
+    #command line Ui menu here, 1. into, 2. out of
+
+
+
+if __name__ == "__main__":
+    main()
