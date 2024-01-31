afd843b7ee4b519c87436a9edc80a49d14122c05
Patrick Viscount <71523753+patrickviscount@users.noreply.github.com>
2024-01-31 10:56:42 -0400

    Update cmdUI.py

--- cmdUI.py
+++ cmdUI.py
@@ -1,6 +1,11 @@
 #This is the main file, it will serve the user a command line user interface to allow them to interact with the software.
 #In the backend, it will talk to the other python file, conversionLibrary to actually handle the currency conversion.
 
+import conversionLibrary
+#How to use the functions in the library
+#result = conversionLibrary.britishPound(100, 1) 
+
+
 def main():
     options = ["American Dollar", "Mexican Peso", "British Pound"]
     print(f"Welcome, what currency do you want to select: {options}!")
